677a9af885c92a29e994002ce08b8b9a12e63170
Sebastian Mazur <s.mazur.praca@gmail.com>
2017-10-17 19:47:05 +0200

    CZat

---
+++ client/App.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import io from 'socket.io-client';
+import styles from './App.css';
+
+import MessageForm from './MessageForm';
+import MessageList from './MessageList';
+import UsersList from './UsersList';
+import UserForm from './UserForm';
+
+const socket = io('/');
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { users: [], messages: [], text: '', name:''};
+    }
+
+    componentDidMount() {
+        socket.on('message', message => this.messageRecive(message));
+        socket.on('update', ({users}) => this.chatUpdate(users));
+    }
+
+    messageRecive(message) {
+        const messages = [message, ...this.state.messages];
+        this.setState({messages});
+    }
+
+    chatUpdate(users) {
+        this.setState({users});
+    }
+
+    handleMessageSubmit(message) {
+        const messages = [message, ...this.state.messages];
+        this.setState({ messages });
+        socket.emit('message', message);
+    }
+
+    handleUserSubmit(name) {
+        this.setState({users});
+        socket.emit('join', name);
+    }
+
+    render() {
+        return this.state.name !== '' ? this.renderLayout() : this.renderUserForm();
+    }
+
+    renderLayout() {
+        return (
+            <div className={styles.App}>
+                <div className={styles.AppHeader}>
+                    <div className={styles.AppTitle}>
+                        ChatApp
+                    </div>            
+                    <div className={styles.AppRoom}>
+                        App room
+                    </div>
+                </div>
+                <div className={styles.AppBody}>
+                    <UserList
+                        users={this.state.users}
+                    />
+                    <div className={styles.MessageWraper}>
+                        <MessageList
+                            messages={this.state.messages}
+                        />
+                        <MessageForm
+                            onMessageSubmit={message => this.handleMessageSubmit(message)}
+                            name={this.state.name}
+                        />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    renderUserForm() {
+        return (<UserForm onUserSubmit={name => this.handleUserSubmit(name)} />)
+    }
+};
+
+export default App;
